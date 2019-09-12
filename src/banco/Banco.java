a68546b7a2556f48bc7b3f8eaf5c907e0e98e289
bianca04022003@gmail.com <alexandre.vieira0203@gmail.com>
2019-09-12 12:22:17 -0300

    Classe pessoa, funcionario e banco atualizados

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,5 +1,41 @@
 package banco;
 
 class Banco {
-    
+    private String nome;
+    private int codBanco;
+    private Funcionario funcionario;
+    private Cliente Cliente;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCodBanco() {
+        return codBanco;
+    }
+
+    public void setCodBanco(int codBanco) {
+        this.codBanco = codBanco;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(Funcionario funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public Cliente getCliente() {
+        return Cliente;
+    }
+
+    public void setCliente(Cliente Cliente) {
+        this.Cliente = Cliente;
+    }
+
 }
--- src/banco/Principal.java
+++ src/banco/Principal.java
@@ -3,6 +3,22 @@ package banco;
 public class Principal {
 
     public static void main(String[] args) {
+        Banco b = new Banco();
+        Cliente c = new Cliente();
+        Funcionario f = new Funcionario();
+        
+        b.setCodBanco(5);
+        b.setNome("Santander");
+        b.setCliente(c);
+        b.setFuncionario(f);
+        
+        f.setCodFuncionario(3);
+        f.setSetor("Administração");
+        f.setBanco(b);
+        
+        c.setCodCliente(10);
+        c.setBanco(b);
+        c.setCredito(10.5);
         
     }
     
